b6c68f20f6df4a04f43050d57e2b31e44dbc6097
Erick Fernando Guzmán Valerio <guzmi.dev@gmail.com>
2021-08-25 17:37:28 -0600

    :zap: Logic task array finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header';
+import FormTask from './componentes/FormTask';
 
 const App = () => {
+  const [task, changeTask] = useState(
+    [
+      {
+        id: 1,
+        texto: 'Lavar la ropa',
+        completada: false
+      },
+      {
+        id: 2,
+        texto: 'Lavar la casa',
+        completada: true
+      },
+    ]
+  );
+
   return (
     <div className="contenedor">
-      <Header></Header>
+      <Header />
+      <FormTask task={task} changeTask={changeTask} />
     </div>
   );
 }
---
+++ src/componentes/FormTask.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+
+const FormTask = ({ task, changeTask }) => {
+  const [inputTask, changeInputTask] = useState('');
+
+
+  const handleInput = (e) => {
+    changeInputTask(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    changeTask(
+      [...task,
+      {
+        id: uuidv4(),
+        texto: inputTask,
+        completada: true
+      }
+      ]
+    );
+  };
+
+  return (
+    <form action="" className="form-task" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-task__input"
+        placeholder="Escribe una tarea"
+        value={inputTask}
+        onChange={(e) => handleInput(e)}
+      />
+      <button
+        type="submit"
+        className="form-task__btn"
+      >
+        <FontAwesomeIcon
+          icon={faPlusSquare}
+          className="form-task__icon-btn" />
+      </button>
+    </form>
+
+  );
+}
+
+export default FormTask;
