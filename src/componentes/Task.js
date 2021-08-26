9ec86ab4d4bf0f53a7e4eec129e8fcb92f6fa65c
Erick Fernando Guzmán Valerio <guzmi.dev@gmail.com>
2021-08-25 18:52:53 -0600

    :zap: Form to change task

--- src/componentes/Task.js
+++ src/componentes/Task.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckSquare, faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Task = ({ task }) => {
+
+  const [editTask, changeEditTask] = useState(false);
+  const [newTask, changeNewTask] = useState(task.text)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    task.text = newTask;
+    changeEditTask(!editTask);
+  };
+
   return (
     <li className="task-list__task">
       <FontAwesomeIcon
@@ -10,12 +21,28 @@ const Task = ({ task }) => {
         className="task-list__icon task-list__icon-check"
       />
       <div className="task-list__text">
-        {task.text}
+        {editTask ?
+          <form action="" className="form-edit-task" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-edit-task__input"
+              value={newTask}
+              onChange={(e) => changeNewTask(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="form-edit-task__btn"
+            >Actualizar</button>
+          </form>
+          :
+          task.text
+        }
       </div>
       <div className=" task-list__container-buttons">
         <FontAwesomeIcon
           icon={faEdit}
           className="task-list__icon task-list__icon-action"
+          onClick={() => changeEditTask(!editTask)}
         />
         <FontAwesomeIcon
           icon={faTimes}
